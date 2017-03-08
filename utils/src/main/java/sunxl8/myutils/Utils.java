b5b72bb0887f1c4b4095993ab80076341757a937
sunxiaolei <xiaoleisun92@gmail.com>
2017-03-08 14:45:54 +0800

    create

---
+++ utils/src/main/java/sunxl8/myutils/Utils.java
@@ -0,0 +1,31 @@
+package sunxl8.myutils;
+
+import android.content.Context;
+
+/**
+ * Utils初始化
+ */
+public class Utils {
+
+    private static Context context;
+
+    /**
+     * 初始化工具类
+     *
+     * @param context 上下文
+     */
+    public static void init(Context context) {
+        Utils.context = context.getApplicationContext();
+    }
+
+    /**
+     * 获取ApplicationContext
+     *
+     * @return ApplicationContext
+     */
+    public static Context getContext() {
+        if (context != null) return context;
+        throw new NullPointerException("u should init first");
+    }
+
+}
